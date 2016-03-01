d0caa73fdbb8fa4ca235385c5d54c614b3b8afd7
Peter Schulam <pschulam@gmail.com>
2016-03-01 11:17:02 -0500

    Fix setup script.

--- runme.sh
+++ runme.sh
@@ -1,4 +1,5 @@
-REPO="https://github.com/VundleVim/Vundle.vim.git"
-DEST="~/.vim/bundle/Vundle.vim"
-
+REPO=https://github.com/VundleVim/Vundle.vim.git
+DEST=~/.vim/bundle/Vundle.vim
+RCFILE=$(pwd)/vimrc
 git clone ${REPO} ${DEST}
+(cd && ln -sv ${RCFILE} .vimrc)
